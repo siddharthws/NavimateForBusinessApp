2662559cd40a2d5e2ea255ccabd6d80bed0f26ed
siddharthg <siddharthg.official@gmail.com>
2017-09-27 15:57:00 +0530

    [CALIBRI][SUPPORT] Add calibri supported elements - Add textview and editext with calibri set as default font - Add styles for making text bold / italic

---
+++ app/src/main/java/com/biz/navimate/views/EtCalibri.java
@@ -0,0 +1,69 @@
+package com.biz.navimate.views;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Typeface;
+import android.support.v7.widget.AppCompatEditText;
+import android.util.AttributeSet;
+
+import com.biz.navimate.R;
+import com.biz.navimate.misc.TypefaceHelper;
+
+/**
+ * Created by Siddharth on 27-09-2017.
+ */
+
+public class EtCalibri extends AppCompatEditText {
+    // ----------------------- Constructors ----------------------- //
+    public EtCalibri(Context context)
+    {
+        super(context);
+        InitView(context, null);
+    }
+
+    public EtCalibri(Context context, AttributeSet attrs)
+    {
+        super(context, attrs);
+        TypedArray attrArray = context.getTheme().obtainStyledAttributes(attrs, R.styleable.Text, 0, 0);
+        InitView(context, attrArray);
+    }
+
+    public EtCalibri(Context context, AttributeSet attrs, int defStyleAttr)
+    {
+        super(context, attrs, defStyleAttr);
+        TypedArray attrArray = context.getTheme().obtainStyledAttributes(attrs, R.styleable.Text, 0, 0);
+        InitView(context, attrArray);
+    }
+
+    // ----------------------- Private APIs ----------------------- //
+    // ----------------------- Private APIs ----------------------- //
+    private void InitView(Context context, TypedArray attrs)
+    {
+        Typeface calibri = TypefaceHelper.GetCalibri(getContext());
+
+        if (calibri == null)
+        {
+            calibri = Typeface.createFromAsset(getContext().getAssets(), "fonts/calibri.otf");
+        }
+
+        // Set font based on style properties
+        if (attrs.getBoolean(R.styleable.Text_bold, false) && (attrs.getBoolean(R.styleable.Text_italic, false)))
+        {
+            setTypeface(calibri, Typeface.BOLD_ITALIC);
+        }
+        else if (attrs.getBoolean(R.styleable.Text_bold, false))
+        {
+            setTypeface(calibri, Typeface.BOLD);
+        }
+        else if (attrs.getBoolean(R.styleable.Text_italic, false))
+        {
+            setTypeface(calibri, Typeface.ITALIC);
+        }
+        else
+        {
+            setTypeface(calibri);
+        }
+
+        setTextColor(getContext().getResources().getColor(android.R.color.black));
+    }
+}
