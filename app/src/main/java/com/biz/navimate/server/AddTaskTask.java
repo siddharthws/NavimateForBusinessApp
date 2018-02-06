09106bc2d1ba4f87bdbe7f102f5b409489590741
jagan241296 <jagan241296@gmail.com>
2018-02-06 18:51:19 +0530

    [UI][TASK]-Send Added task to server --Synced Task with server after successful creation.

---
+++ app/src/main/java/com/biz/navimate/server/AddTaskTask.java
@@ -0,0 +1,116 @@
+package com.biz.navimate.server;
+
+import android.content.Context;
+import android.widget.Toast;
+
+import com.biz.navimate.constants.Constants;
+import com.biz.navimate.database.DbHelper;
+import com.biz.navimate.debug.Dbg;
+import com.biz.navimate.objects.Dialog;
+import com.biz.navimate.objects.Field;
+import com.biz.navimate.objects.Value;
+import com.biz.navimate.views.RlDialog;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+import okhttp3.RequestBody;
+
+/**
+ * Created by Sai_Kameswari on 06-02-2018.
+ */
+
+public class AddTaskTask extends BaseServerTask {
+    // ----------------------- Constants ----------------------- //
+    private static final String TAG = "ADD_TASK_TASK";
+
+    // ----------------------- Classes ---------------------------//
+    // ----------------------- Globals ----------------------- //
+    private long leadId;
+    private long formTemplateId;
+    private long templateId;
+    private ArrayList<Value> values;
+
+    // ----------------------- Constructor ----------------------- //
+    public AddTaskTask(Context parentContext, long leadId, long formTemplateId, long templateId, ArrayList<Value> values)
+    {
+        super(parentContext, Constants.Server.URL_ADD_TASK);
+        this.leadId = leadId;
+        this.formTemplateId = formTemplateId;
+        this.templateId = templateId;
+        this.values=values;
+    }
+
+    // ----------------------- Overrides ----------------------- //
+    @Override
+    public void onPreExecute ()
+    {
+        RlDialog.Show(new Dialog.Waiting("Adding Task..."));
+    }
+
+    @Override
+    public Void doInBackground (Void... params)
+    {
+        // Init Request JSON
+        JSONObject requestJson = new JSONObject();
+        try
+        {
+            requestJson.put(Constants.Server.KEY_LEAD_ID, leadId);
+            requestJson.put(Constants.Server.KEY_FORM_TEMPLATE_ID, formTemplateId);
+            requestJson.put(Constants.Server.KEY_TEMPLATE_ID, templateId);
+
+            JSONArray valuesJson = new JSONArray();
+            for (Value value : values) {
+                // Get Value and Field
+                Field field = (Field) DbHelper.fieldTable.GetById(value.fieldId);
+
+                // Create Value JSON
+                JSONObject valueJson = new JSONObject();
+                valueJson.put(Constants.Server.KEY_FIELD_ID, field.serverId);
+                valueJson.put(Constants.Server.KEY_VALUE, value.value);
+
+                // Add to Values JSON Array
+                valuesJson.put(valueJson);
+            }
+            // Create Data JSON
+            JSONObject dataJson = new JSONObject();
+            dataJson.put(Constants.Server.KEY_TEMPLATE_ID, templateId);
+            dataJson.put(Constants.Server.KEY_VALUES, valuesJson);
+
+            requestJson.put(Constants.Server.KEY_TEMPLATE_DATA, dataJson);
+        }
+        catch (JSONException e)
+        {
+            Dbg.error(TAG, "Error while putting initData in JSON");
+            return null;
+        }
+
+        // Add data to request Builder
+        reqBuilder.method("POST", RequestBody.create(JSON, requestJson.toString()));
+
+        // Call Super
+        super.doInBackground(params);
+
+        return null;
+    }
+    @Override
+    public void onPostExecute (Void result)
+    {
+        RlDialog.Hide();
+        if (IsResponseValid())
+        {
+            Dbg.Toast(parentContext,"Task Created Successfully", Toast.LENGTH_SHORT);
+            SyncDbTask syncDbTask = new SyncDbTask(parentContext, false);
+            syncDbTask.execute();
+        }
+        else
+        {
+            Dbg.Toast(parentContext,"Task Creation Failed", Toast.LENGTH_SHORT);
+        }
+    }
+    // ----------------------- Public APIs ----------------------- //
+    // ----------------------- Private APIs ----------------------- //
+}
