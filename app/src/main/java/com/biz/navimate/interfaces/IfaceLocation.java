c2089dd5020c976087df17af29e75a9fb7635f45
siddharthg <siddharthg.official@gmail.com>
2018-05-25 17:34:01 +0530

    [MOVEMENT][UPDATE] Add movement type based on current speed of user - Add logic to generate movement type in location object using current speed - Add movement update interface and trigger the listener when new location's movement type changes

---
+++ app/src/main/java/com/biz/navimate/interfaces/IfaceLocation.java
@@ -0,0 +1,8 @@
+package com.biz.navimate.interfaces;
+
+public class IfaceLocation {
+    // GetProfile Task interface
+    public interface MovementUpdate {
+        void onMovementUpdated();
+    }
+}
