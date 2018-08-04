6da6aa6c6ccccc24865c4d800a1e15e6a4fabdbb
siddharthg <siddharthg.official@gmail.com>
2018-08-04 13:48:46 +0530

    [CLEANUP] Make develop branch as debuggable by default

--- app/src/main/java/com/biz/navimate/constants/Constants.java
+++ app/src/main/java/com/biz/navimate/constants/Constants.java
@@ -8,7 +8,7 @@ public class Constants {
     // General App Constants
     public static class App {
         // Toggle debug mode
-        public static final Boolean DEBUG        = false;
+        public static final Boolean DEBUG        = true;
     }
 
     // Request codes for activity results
