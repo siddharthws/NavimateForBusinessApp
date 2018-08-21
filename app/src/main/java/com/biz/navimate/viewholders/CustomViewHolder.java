c7c19b0c570b51e081007b69dd2578c76243c559
siddharthg <siddharthg.official@gmail.com>
2018-08-21 21:11:50 +0530

    [EDITTEXT][CLEARABLE] Add support for clearable edit text - Add a cross button with edit text to clear the edit text anytime - Create a compound view for clearable edittext - Add custom attribute to set input type of edit text in compound view

--- app/src/main/java/com/biz/navimate/viewholders/CustomViewHolder.java
+++ app/src/main/java/com/biz/navimate/viewholders/CustomViewHolder.java
@@ -3,10 +3,17 @@ package com.biz.navimate.viewholders;
 import android.widget.ImageButton;
 
 import com.biz.navimate.views.TvCalibri;
+import com.biz.navimate.views.core.NvmImageButton;
+import com.biz.navimate.views.custom.NvmEditText;
 
 public class CustomViewHolder {
     public static class DateEditor {
         public ImageButton ibEdit, ibClear = null;
         public TvCalibri tvText = null;
+    }
+
+    public static class EtClearable {
+        public NvmEditText etText = null;
+        public NvmImageButton ibClear = null;
     }
 }
