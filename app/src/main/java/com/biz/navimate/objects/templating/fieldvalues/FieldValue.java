f73d568673cd23b4f00b69d22432e33b7decf4af
siddharthg <siddharthg.official@gmail.com>
2018-09-05 18:32:45 +0530

    [FIELD][VIEW][SUPPORT] Add support for field views - Add FieldValue object which would form the base for all specific field value classes -- Base class would help initializing field value based on field and String value -- Base class would provide method to validate the value - Add FieldView which would form the basis for all specific field views -- Base class will inflate and initialize field views, provide public APIs to set/get value, editability and display error -- Base class will have static method to initialize field view based on field value - Add FieldViewHolder which would form the base for all field view holder classes

---
+++ app/src/main/java/com/biz/navimate/objects/templating/fieldvalues/FieldValue.java
@@ -0,0 +1,35 @@
+package com.biz.navimate.objects.templating.fieldvalues;
+
+import com.biz.navimate.objects.Field;
+
+public abstract class FieldValue {
+    // ----------------------- Constants ----------------------- //
+    private static final String TAG = "FIELD_VALUE";
+
+    // ----------------------- Abstracts ----------------------- //
+    // String converter methods
+    public abstract String  toString    ();
+    public abstract void    fromString  (String value);
+
+    // Method to check for error in current input data in the editor
+    // Returns blank string if no error found
+    public abstract String  getError    ();
+
+    // ----------------------- Globals ----------------------- //
+    public Field field = null;
+
+    // ----------------------- Constructor ----------------------- //
+    public FieldValue(Field field) {
+        this.field = field;
+    }
+
+    // ----------------------- Static Methods ----------------------- //
+    // Method to parse field and value into FieldValue
+    public static FieldValue newInstance(Field field, String value) {
+        switch (field.type) {
+            // Placeholder
+        }
+
+        return null;
+    }
+}
